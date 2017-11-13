4728b84fb0540c667b3cf6a3b95cd41f2ddc1356
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2017-11-13 21:23:03 +0530

    Fixing run.sh file

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 ./HFO/bin/HFO --offense-npcs 2 --defense-npcs 1 --defense-agents 1 &
 sleep 5
-./di_sarsa/di_sarsa --numAgents 1 --numOpponents 2 --step 10 &
+cd di_sarsa
+./di_sarsa --numAgents 1 --numOpponents 2 --step 10 &
