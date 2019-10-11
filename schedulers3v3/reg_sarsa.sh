bf1be9caea41c8f517166f259e178685c233bcb9
virtualgod <virtualgod@github.com>
2019-10-10 22:48:38 -0600

    3v3 scripts

---
+++ schedulers3v3/reg_sarsa.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+stdbuf -oL ./HFO/bin/HFO --offense-npcs 3 --defense-npcs 1 --defense-agents 2 \
+--port 7135 --no-logging --headless --trials 50000 --seed 1 > logs/reg_sarsa.log 2>&1 &
+
+PID=$!
+cd reg_sarsa
+sleep 5
+
+./reg_sarsa --numAgents 2 --numOpponents 3 --numEpisodes 50000 --basePort 7135 \
+--weightId reg_sarsa_lambda_0.95_regReward_2.0_seed_1 --lambda 0.95 --regReward 2.0
+
+kill -SIGINT $PID
+sleep 5
+cd ..
